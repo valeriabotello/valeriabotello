d3314ebbb0e675bce5cfb506d413469d5930f1f2
valeriabotello <valeria.botello.olvera2410@gmail.com>
2024-11-07 00:10:32 +0000

    coffe

---
+++ app.vue
@@ -0,0 +1,55 @@
+<script setup>
+import planItem from './components/plan-item.vue';
+</script>
+
+<template>
+  <header>
+    <div style="text-align: center;">
+      <span id="logo">ITGAM BOX EXPERIENCES</span> <br>
+      <img src="./assets/logo.png" alt="logo">
+    </div>
+  </header>
+
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+
+    <h2 class="subtitle">
+      Viajamos por el mundo para encontrar el mejor café de origen único para ti
+    </h2>
+
+    <div class="plans">
+      <plan-item />
+      <plan-item />
+      <plan-item />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
